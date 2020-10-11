6a513730a4f9d8514a514fb64ef3d9b4af668a74
fengyinchao <fengyinchao@fenbi.com>
2020-10-11 22:31:56 +0800

    feat: add use-onpush rule in angular

---
+++ lib/src/rules/use-onpush.d.ts
@@ -0,0 +1,8 @@
+export declare const meta: {
+    docs: {
+        description: string;
+        category: string;
+        recommended: boolean;
+    };
+    fixable: string;
+}, create: (context: any) => Record<string, unknown>;
