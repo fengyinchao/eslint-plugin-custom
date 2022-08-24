a70e194426615661f398585054cb2d2ee9534a03
冯银超 <fengzhihuashen@douyu.tv>
2022-08-24 19:11:08 +0800

    feat: add rule scrollview-flatlist-sectionlist-use-scrollIndicatorInsets and style-generate

---
+++ lib/src/rules/scrollview-flatlist-sectionlist-use-scrollIndicatorInsets.d.ts
@@ -0,0 +1,8 @@
+export declare const meta: {
+    type: string;
+    docs: {
+      description: string;
+    };
+    fixable: string;
+  },
+  create: (context: any) => Record<string, unknown>;
---
+++ lib/src/rules/style-generate.d.ts
@@ -0,0 +1,9 @@
+export declare const meta: {
+    docs: {
+      description: string;
+      category: string;
+      recommended: boolean;
+    };
+    fixable: string;
+  },
+  create: (context: any) => Record<string, unknown>;
